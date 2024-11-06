ab523540cd3849760505a67ad01e9235399f788c
Martin Rioja <cr.martin.24680@gmail.com>
2024-11-06 00:07:12 -0500

    Delete ArquiWeb_SI62_G6-CRUD-con-CORS directory

--- ArquiWeb_SI62_G6-CRUD-con-CORS/src/main/java/pe/edu/upc/project_security_g06/repositories/IRoleRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.project_security_g06.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pe.edu.upc.project_security_g06.entities.Role;
-
-@Repository
-public interface IRoleRepository extends JpaRepository<Role, Long> {
-}
