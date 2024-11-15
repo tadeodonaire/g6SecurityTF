34293a9e8311629fbfba3cbd5064a58a6b30398d
Tadeo Donaire <116527582+tadeodonaire@users.noreply.github.com>
2024-11-14 23:04:38 -0500

    seguridad

--- src/app/components/navegacion/navegacion.component.ts
+++ src/app/components/navegacion/navegacion.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { LoginService } from '../../services/login.service';
+import {MatSidenav, MatSidenavModule} from '@angular/material/sidenav';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navegacion',
   standalone: true,
-  imports: [RouterOutlet,MatIcon,MatMenuModule,MatToolbarModule,RouterModule],
+  imports: [RouterOutlet,MatIcon,MatMenuModule,MatToolbarModule,RouterModule,MatSidenavModule, CommonModule],
   templateUrl: './navegacion.component.html',
   styleUrl: './navegacion.component.css'
 })
@@ -24,12 +26,16 @@ export class NavegacionComponent {
     this.role = this.loginService.showRole();
     return this.loginService.verificar();
   }
-  isDeveloper() {
+  isAdministrador() {
     return this.role === 'ADMINISTRADOR';
   }
 
-  isTester() {
-    return this.role === 'PERSONAL';
+  isPersonal() {
+    return this.role === 'PERSONALEMERGENCIA';
+  }
+
+  isUsuario() {
+    return this.role === 'USUARIO';
   }
 
 }
