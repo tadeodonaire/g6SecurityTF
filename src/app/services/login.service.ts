6e8e1dbce9fa3a05029a16af829d91a7e12aa5e5
Tadeo Donaire <116527582+tadeodonaire@users.noreply.github.com>
2024-11-17 21:12:25 -0500

    Seguridad 1.11

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -7,33 +7,20 @@ import { JwtHelperService } from '@auth0/angular-jwt';
   providedIn: 'root'
 })
 export class LoginService {
-  constructor(private http: HttpClient) { }
-  
+  constructor(private http: HttpClient) {}
   login(request: JwtRequest) {
     return this.http.post('http://localhost:8082/login', request);
   }
-  
   verificar() {
-    if (typeof sessionStorage !== "undefined") {
-      let token = sessionStorage.getItem('token');
-      return token != null;
-    }
-    console.warn("sessionStorage no está disponible en este entorno.");
-    return false;
+    let token = sessionStorage.getItem('token');
+    return token != null;
   }
-  
   showRole() {
-    if (typeof sessionStorage === "undefined") {
-      console.warn("sessionStorage no está disponible en este entorno.");
-      return null;
-    }
-
     let token = sessionStorage.getItem('token');
     if (!token) {
       // Manejar el caso en el que el token es nulo.
       return null; // O cualquier otro valor predeterminado dependiendo del contexto.
     }
-    
     const helper = new JwtHelperService();
     const decodedToken = helper.decodeToken(token);
     return decodedToken?.role;
