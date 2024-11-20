6d8c74ca5399c78bd0a041d99e7afc17d696f891
Tadeo Donaire <116527582+tadeodonaire@users.noreply.github.com>
2024-11-19 22:33:01 -0500

    prueba2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 
 export const environment ={
     production:false,
-    base:"http://security-db.postgres.database.azure.com/Grupo6_app?useSSL=true&requireSSL=true&verifyServerCertificate=true"
+    base:"https://arquitecturaweb.azurewebsites.net"
 
 }
